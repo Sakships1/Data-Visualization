b3e31fe43829e99bac2d5f343ba1b251010115fb
2129626@cognizant.com <2129626@cognizant.com>
2024-03-20 16:01:31 +0530

    adding js basics

---
+++ Javascript here/index.js
@@ -0,0 +1,32 @@
+alert("Hello, Kaha ja rhe ho? Baith k padho.")
+
+console.log(100);
+
+// ctrl + /
+
+console.log(20, 'Hello', true);
+
+console.log(1)
+setTimeout(() =>{
+console.log(2)
+}, 10)
+setTimeout(() =>{
+console.log(3)
+}, 0)
+console.log(4)
+
+console.error('alert');
+
+const styles = 'padding: 10px; background-color: white; color: green';
+console.log('Hello World', styles)
+
+
+// Ways to declare a variable
+
+let fname = "Sakshi";
+let lname = "Gupta";
+console.log(fname, lname);
+
+const arr = [1, 2, 3, 4]
+arr.push(5);
+console.log(arr);
